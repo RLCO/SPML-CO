0f476298863232c282765093f329941832449936
telenocodoo <45532030+telenocodoo@users.noreply.github.com>
2019-08-04 13:25:33 +0300

    Add files via upload

--- spml_tender_sales/models/tender_sales.py
+++ spml_tender_sales/models/tender_sales.py
@@ -1,8 +1,118 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+# from odoo import models, fields, api, _
+# from odoo.exceptions import UserError
 
+
+from odoo import api, fields, models, _, SUPERUSER_ID
+# from odoo.osv import expression
+# from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+from odoo.exceptions import UserError
+# from odoo.addons.stock.models.stock_move import PROCUREMENT_PRIORITIES
+
+
+
+class TenderSalesDelivery(models.Model):
+    _inherit ='stock.picking'
+
+    def add_done_qty(self,line_ids,pid,qty):
+        for rec in line_ids:
+            print(rec.tender_id, rec.product_id, rec.delivered_quantity, rec.balance)
+            if rec.product_id ==pid :
+                rec.delivered_quantity += qty
+    @api.multi
+    def button_validate(self):
+        stock_sale_id = self.sale_id.id
+        tender_obj = self.env['tender.sales'].search([('sale_id', '=', stock_sale_id)])
+        tender_line_ids = self.env['tender.sales.lines'].search([('tender_id', '=', tender_obj.id)])
+
+        # print(stock_sale_id)
+
+        #
+        self.ensure_one()
+        if not self.move_lines and not self.move_line_ids:
+            raise UserError(_('Please add some items to move.'))
+        # else :
+
+
+        # If no lots when needed, raise error
+        picking_type = self.picking_type_id
+        precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        no_quantities_done = all(float_is_zero(move_line.qty_done, precision_digits=precision_digits) for move_line in
+                                 self.move_line_ids.filtered(lambda m: m.state not in ('done', 'cancel')))
+        no_reserved_quantities = all(
+            float_is_zero(move_line.product_qty, precision_rounding=move_line.product_uom_id.rounding) for move_line in
+            self.move_line_ids)
+        if no_reserved_quantities and no_quantities_done:
+            raise UserError(_(
+                'You cannot validate a transfer if no quantites are reserved nor done. To force the transfer, switch in edit more and encode the done quantities.'))
+
+        if picking_type.use_create_lots or picking_type.use_existing_lots:
+            lines_to_check = self.move_line_ids
+            if not no_quantities_done:
+                lines_to_check = lines_to_check.filtered(
+                    lambda line: float_compare(line.qty_done, 0,
+                                               precision_rounding=line.product_uom_id.rounding)
+                )
+
+            for line in lines_to_check:
+                product = line.product_id
+                if product and product.tracking != 'none':
+                    if not line.lot_name and not line.lot_id:
+                        raise UserError(
+                            _('You need to supply a Lot/Serial number for product %s.') % product.display_name)
+
+        if no_quantities_done:
+            view = self.env.ref('stock.view_immediate_transfer')
+            wiz = self.env['stock.immediate.transfer'].create({'pick_ids': [(4, self.id)]})
+            return {
+                'name': _('Immediate Transfer?'),
+                'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'stock.immediate.transfer',
+                'views': [(view.id, 'form')],
+                'view_id': view.id,
+                'target': 'new',
+                'res_id': wiz.id,
+                'context': self.env.context,
+            }
+
+        if self._get_overprocessed_stock_moves() and not self._context.get('skip_overprocessed_check'):
+            view = self.env.ref('stock.view_overprocessed_transfer')
+            wiz = self.env['stock.overprocessed.transfer'].create({'picking_id': self.id})
+            return {
+                'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'stock.overprocessed.transfer',
+                'views': [(view.id, 'form')],
+                'view_id': view.id,
+                'target': 'new',
+                'res_id': wiz.id,
+                'context': self.env.context,
+            }
+
+        # Check backorder should check for other barcodes
+        if self._check_backorder():
+            for rec in self.move_line_ids:
+                print(rec.qty_done, rec.product_id, rec.move_id.name, rec.move_id.product_uom_qty)
+                self.add_done_qty(tender_line_ids, rec.product_id, rec.qty_done)
+                print(rec.qty_done, rec.product_id, rec.move_id.name, rec.move_id.product_uom_qty)
+
+             print("hi... Validate haytham")
+            return self.action_generate_backorder_wizard()
+        self.action_done()
+        return
+
+#
+# for rec in self.move_line_ids:
+#     print(rec.qty_done, rec.product_id, rec.move_id.name, rec.move_id.product_uom_qty)
+#     self.add_done_qty(tender_line_ids, rec.product_id, rec.qty_done)
+#     print(rec.qty_done, rec.product_id, rec.move_id.name, rec.move_id.product_uom_qty)
+#
+# print("hi... Validate haytham")
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -10,6 +120,25 @@ class SaleOrder(models.Model):
     is_tender = fields.Boolean()
     period = fields.Selection(selection=[('weekly', 'weekly'), ('monthly', 'monthly'), ])
     tender_id = fields.Many2one("tender.sales")
+
+    #This will over ride delivery button haytham
+    @api.multi
+    def action_view_delivery(self):
+        '''
+        This function returns an action that display existing delivery orders
+        of given sales order ids. It can either be a in a list or in a form
+        view, if there is only one delivery order to show.
+        '''
+        action = self.env.ref('stock.action_picking_tree_all').read()[0]
+
+        pickings = self.mapped('picking_ids')
+        print("hi ... haytham")
+        if len(pickings) > 1:
+            action['domain'] = [('id', 'in', pickings.ids)]
+        elif pickings:
+            action['views'] = [(self.env.ref('stock.view_picking_form').id, 'form')]
+            action['res_id'] = pickings.id
+        return action
 
     @api.one
     @api.returns('self', lambda value: value.id)
@@ -25,6 +154,7 @@ class SaleOrder(models.Model):
         tender_id = self.env['tender.sales']
         tender_line_id = self.env['tender.sales.lines']
         invoice_id = self.env['account.invoice'].search([('origin', '=', self.name)], limit=1)
+
         for record in self:
             tender_obj = tender_id.create({
                 'sale_id': self.id,
@@ -199,6 +329,26 @@ class TenderSales(models.Model):
     invoice_id = fields.Many2one("account.invoice")
     tender_ids = fields.One2many("tender.sales.lines", "tender_id")
     period = fields.Selection(selection=[('weekly', 'weekly'), ('monthly', 'monthly'), ])
+
+
+    # This will over ride delivery button haytham
+    @api.multi
+    def action_view_delivery(self):
+        '''
+        This function returns an action that display existing delivery orders
+        of given sales order ids. It can either be a in a list or in a form
+        view, if there is only one delivery order to show.
+        '''
+        action = self.env.ref('stock.action_picking_tree_all').read()[0]
+
+        pickings = self.mapped('picking_ids')
+        print("hi ... haytham sales tender")
+        if len(pickings) > 1:
+            action['domain'] = [('id', 'in', pickings.ids)]
+        elif pickings:
+            action['views'] = [(self.env.ref('stock.view_picking_form').id, 'form')]
+            action['res_id'] = pickings.id
+        return action
 
     @api.multi
     def transfer_quantity_to_product(self):
