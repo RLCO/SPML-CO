9ecaef7f2589d041680dce7634e45ff22278bebe
telenocodoo <45532030+telenocodoo@users.noreply.github.com>
2019-11-07 13:10:45 +0500

    Add files via upload

---
+++ purchase_request_report/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "purchase_request",
+    'summary': """
+        purchase_request""",
+    'description': """
+        purchase_request
+    """,
+    'author': "Magdy, helcon",
+    'website': "http://www.yourcompany.com",
+    'category': 'purchase',
+    'version': '0.1',
+    'depends': ['web', 'sprogroup_purchase_request'],
+    'data': [
+        'report/purchase_request.xml',
+    ],
+}
